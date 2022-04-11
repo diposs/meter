b4a6002eeb9e77c3fe3b478f3fbbd8f25a7dbc9d
diposs <onakunle.dipo@gmail.com>
2022-04-11 21:30:45 +0100

    initial

---
+++ config.js
@@ -0,0 +1,11 @@
+export const nftaddress = "0x5781E63e50bB76df856dCE70a28efd0ee8E7c53E";
+export const nftmarketaddress = "0xbf7Ce9E3F66d1729865813267AA68662A4eE44FC";
+//EDIT !!!
+let x = 1;
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
